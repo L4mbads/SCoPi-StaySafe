e876b12fddf5105cf309bd6d2df383ce0657f29e
Filbert Engyo <filbert.engyo@gmail.com>
2025-09-26 17:42:11 +0700

    refactor: admin homepage

--- frontend/src/app/admin/home/page.tsx
+++ frontend/src/app/admin/home/page.tsx
@@ -9,7 +9,7 @@ export default function AdminHome() {
       {/* Content with sidebar + main area */}
       <div className="flex pt-26">
         {/* Sidebar */}
-        <aside className="w-64 bg-white shadow-md p-6 fixed ">
+        <aside className="w-64 bg-white shadow-md p-6">
           <div className="flex items-center space-x-2 mb-8">
             <span className="text-indigo-700">
               <svg
